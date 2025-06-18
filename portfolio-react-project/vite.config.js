e900341e832bbaa3df7bb46a7d24bd65408d4b0d
Benjamin James Mulbah <mulbahb997@gmail.com>
2025-06-18 08:09:46 +0200

    fixed some bugs

--- portfolio-react-project/vite.config.js
+++ portfolio-react-project/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
-    react()
   ],
-  base: "/portfolio-react"
-});
+  base: '/portfolio-react/', // important for GitHub Pages
+})
