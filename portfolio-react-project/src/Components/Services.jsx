606a27360419570b9d52fa2464a1ee0e2f33b374
Benjamin James Mulbah <mulbahb997@gmail.com>
2025-06-18 09:48:41 +0200

    changes

--- portfolio-react-project/src/Components/Services.jsx
+++ portfolio-react-project/src/Components/Services.jsx
@@ -32,8 +32,10 @@ const Services = () => {
           <span className="flex h-12 w-12 bg-[var(--primary-color)] rounded-full justify-center items-center mb-4">
             <FaPenNib className="text-2xl text-white " />
           </span>
-          <p  data-aos="fade-up" className="mb-4 font-bold">UI/UX Design</p>
-          <p  data-aos="fade-up">
+          <p data-aos="fade-up" className="mb-4 font-bold">
+            UI/UX Design
+          </p>
+          <p data-aos="fade-up">
             I specialize in creating clean, user-friendly, and responsive
             interface designs using tools like Figma. My approach ensures every
             product is both visually appealing and easy to navigate across all
@@ -43,11 +45,13 @@ const Services = () => {
             <ul className="list-disc pl-4">
               <li data-aos="fade-left"> Mobile and responsive layouts</li>
 
-              <li  data-aos="fade-left">Wireframes and interactive prototypes (Figma)</li>
+              <li data-aos="fade-left">
+                Wireframes and interactive prototypes (Figma)
+              </li>
 
-              <li  data-aos="fade-left">High-fidelity UI designs</li>
+              <li data-aos="fade-left">High-fidelity UI designs</li>
 
-              <li  data-aos="fade-left">Basic user journey mapping</li>
+              <li data-aos="fade-left">Basic user journey mapping</li>
             </ul>
           </p>
           <a
@@ -65,23 +69,36 @@ const Services = () => {
           <span className="flex h-12 w-12 bg-[var(--primary-color)] rounded-full justify-center items-center mb-4">
             <FaCode className="text-2xl text-white " />
           </span>
-          <p className="mb-4 font-bold">Web Development</p>
-          <p>
+          <p data-aos="fade-up" className="mb-4 font-bold">
+            Web Development
+          </p>
+          <p data-aos="fade-up">
             I craft responsive, user-friendly websites and web apps using modern
             technologies like React, Tailwind CSS, and Node.js. My focus is on
             performance, accessibility, and clean code.
             <br />
             <strong className="">Deliverables include:</strong>
             <ul className="list-disc pl-4">
-              <li> Fully responsive websites and web applications</li>
+              <li data-aos="fade-left">
+                {" "}
+                Fully responsive websites and web applications
+              </li>
 
-              <li> Clean, maintainable, and scalable code</li>
+              <li data-aos="fade-left">
+                {" "}
+                Clean, maintainable, and scalable code
+              </li>
 
-              <li> Frontend using React & Tailwind CSS</li>
+              <li data-aos="fade-left"> Frontend using React & Tailwind CSS</li>
 
-              <li>Backend integration with Node.js & Express</li>
-              <li> Cross-browser compatibility and mobile-first design</li>
-              <li> Deployment and hosting support</li>
+              <li data-aos="fade-left">
+                Backend integration with Node.js & Express
+              </li>
+              <li data-aos="fade-left">
+                {" "}
+                Cross-browser compatibility and mobile-first design
+              </li>
+              <li data-aos="fade-left"> Deployment and hosting support</li>
             </ul>
           </p>
 
@@ -100,20 +117,34 @@ const Services = () => {
           <span className="flex h-12 w-12 bg-[var(--primary-color)] rounded-full justify-center items-center mb-4">
             <SiMaterialdesignicons className="text-2xl text-white " />
           </span>
-          <p className="mb-4 font-bold">Graphic Design</p>
-          <p>
+          <p data-aos="fade-up" className="mb-4 font-bold">
+            Graphic Design
+          </p>
+          <p data-aos="fade-up">
             I create visually engaging designs that communicate ideas
             effectively. My work includes branding, social media content, and
             marketing materials tailored to your audience.
             <br />
             <strong className="">Deliverables include:</strong>
             <ul className="list-disc pl-4">
-              <li> Logos, banners, and brand identity designs</li>
-              <li> Social media post templates and ads</li>
-              <li> Flyers, posters,brochures, YouTube Banner</li>
-              <li>Business cards and presentation slides</li>
-              <li>Custom illustrations and vector graphics</li>
-              <li>Final files in multiple formats (PNG, JPEG, PDF, SVG)</li>
+              <li data-aos="fade-left">
+                {" "}
+                Logos, banners, and brand identity designs
+              </li>
+              <li data-aos="fade-left"> Social media post templates and ads</li>
+              <li data-aos="fade-left">
+                {" "}
+                Flyers, posters,brochures, YouTube Banner
+              </li>
+              <li data-aos="fade-left">
+                Business cards and presentation slides
+              </li>
+              <li data-aos="fade-left">
+                Custom illustrations and vector graphics
+              </li>
+              <li data-aos="fade-left">
+                Final files in multiple formats (PNG, JPEG, PDF, SVG)
+              </li>
             </ul>
           </p>
 
@@ -132,8 +163,10 @@ const Services = () => {
           <span className="flex h-12 w-12 bg-[var(--primary-color)] rounded-full justify-center items-center mb-4">
             <IoDocumentsOutline className="text-2xl text-white " />
           </span>
-          <p className="mb-4 font-bold">Modern CV & Résumé Design</p>
-          <p>
+          <p data-aos="fade-up" className="mb-4 font-bold">
+            Modern CV & Résumé Design
+          </p>
+          <p data-aos="fade-up">
             I craft professional, clean, and modern CVs from scratch that help
             you stand out. Whether you're applying for jobs, internships, or
             academic opportunities, I’ll design a résumé that highlights your
@@ -141,10 +174,19 @@ const Services = () => {
             <br />
             <strong className="">Deliverables include:</strong>
             <ul className="list-disc pl-4">
-              <li> Custom-designed CV tailored to your field</li>
-              <li> Visually appealing and modern design</li>
-              <li> Editable file (Word, PDF, or Canva/Figma link)</li>
-              <li>Cover letter template </li>
+              <li data-aos="fade-left">
+                {" "}
+                Custom-designed CV tailored to your field
+              </li>
+              <li data-aos="fade-left">
+                {" "}
+                Visually appealing and modern design
+              </li>
+              <li data-aos="fade-left">
+                {" "}
+                Editable file (Word, PDF, or Canva/Figma link)
+              </li>
+              <li data-aos="fade-left">Cover letter template </li>
             </ul>
           </p>
 
